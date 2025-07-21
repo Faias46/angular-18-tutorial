ab71d84680a2f40fe8ee6358a1daa2cb0eb4b282
Faias46 <nagurfaias@gmail.com>
2025-07-21 15:54:59 +0530

    updated data binding code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { EmployeeListComponent } from './components/employee-list/employee-list.component';
-import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
-import { DataBindingComponent } from './components/data-binding/data-binding.component';
+// import { RouterOutlet } from '@angular/router';
+// import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+// import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+// import { DataBindingComponent } from './components/data-binding/data-binding.component';
+import { StructuralDirComponent } from './components/structural-dir/structural-dir.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,EmployeeListComponent,AddEmployeeComponent,DataBindingComponent],
+   imports: [//RouterOutlet,EmployeeListComponent,AddEmployeeComponent,DataBindingComponent,
+    StructuralDirComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/structural-dir/structural-dir.component.ts
@@ -0,0 +1,55 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-structural-dir',                      // Selector for this component
+  standalone: true,                                   // Standalone component declaration
+  imports: [CommonModule, FormsModule],               // Importing required modules for template binding
+  templateUrl: './structural-dir.component.html',     // HTML template file
+  styleUrl: './structural-dir.component.css'          // CSS styling file
+})
+export class StructuralDirComponent {
+
+  // Controls visibility of Div-1
+  isDiv1Visible: boolean = true;
+
+  // Controls visibility of Div-2
+  isDiv2visible: boolean = false;
+
+  // Bound to input fields for numeric comparison in Div-3
+  num1: string = '';
+  num2: string = '';
+
+  
+  isActive: boolean = false; // Controls visibility of Div-4 based on checkboxs
+  
+
+  // Bound to dropdown selection to control Div-4 visibility
+  selectedState: string = '';
+
+  cityArray:string[]=['pune','mumbai','nagpur','thane'];
+
+  studentList:any[]=[
+    {studId:1, name:'aaa',city:'pune',isActive:false },
+    {studId:2, name:'bbb',city:'mumbai',isActive:true },
+    {studId:3, name:'ccc',city:'nagpur',isActive:false },
+    {studId:4, name:'ddd',city:'thane',isActive:true },
+    {studId:5, name:'eee',city:'mumbai',isActive:false },
+  ]
+
+  // Method to show Div-1
+  showDiv1() {
+    this.isDiv1Visible = true;
+  }
+
+  // Method to hide Div-1
+  hideDiv2() {
+    this.isDiv1Visible = false;
+  }
+
+  // Toggles visibility of Div-2
+  togglediv2() {
+    this.isDiv2visible = !this.isDiv2visible;
+  }
+}
